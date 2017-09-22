0bffcf1b8a2406c2da4f4e9b48c9cf53540cc424
Ryan Forgie <ryan.j.forgie@gmail.com>
2017-09-22 17:14:24 +0100

    add visual counter on plaActivity

--- app/src/main/java/com/example/ryanforgie/javalistproject/PlanActivity.java
+++ app/src/main/java/com/example/ryanforgie/javalistproject/PlanActivity.java
@@ -4,11 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 
 public class PlanActivity extends AppCompatActivity {
+
+    TextView counterView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,22 @@ public class PlanActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.week);
         listView.setAdapter(planAdapter);
 
+        Tracker tracker = new Tracker();
+
+        counterView = (TextView) findViewById(R.id.counter_view);
+        counterView.setText(Integer.toString(tracker.getCount()));
 
     }
+
+    ToggleButton toggle = (ToggleButton) findViewById(R.id.togglebutton);
+    toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (isChecked) {
+                // The toggle is enabled
+            } else {
+                // The toggle is disabled
+            }
+        }
+    });
 
 }
---
+++ app/src/main/java/com/example/ryanforgie/javalistproject/Tracker.java
@@ -0,0 +1,20 @@
+package com.example.ryanforgie.javalistproject;
+
+import java.net.Inet4Address;
+
+/**
+ * Created by ryanforgie on 22/09/2017.
+ */
+
+public class Tracker {
+
+    private int count;
+
+    public Tracker() {
+        this.count = 0;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+}
